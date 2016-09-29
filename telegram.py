48f457a13a5162cbfbd3a036d2a49b8b3241ed51
Lorenzo Pieri <lorenzopieri.public@gmail.com>
2016-09-29 16:57:17 +0200

    Changes

--- telegram.py
+++ telegram.py
@@ -26,7 +26,11 @@ cursor = dbconn.cursor()
 #for row in results: dateandtime = row[0] sensor = row[1] temperature = row[2] humidity = row[3]
 #try: cursor.execute(sql_query) except: print 'error'
 
-#Global variables
+
+#Globals
+state = 0
+#state 0 -> /start
+#state 1..10 -> S1..S10 Selected
 sensor_choice = "Standard"
 
 
@@ -35,47 +39,102 @@ def handle(msg):
 	content_type, chat_type, chat_id = telepot.glance(msg)
 	
 	command = msg['text']
-	
+	start_message = "Scegli un'opzione:\n<b>S1</b> sta per Sensore 1\n<b>Mappa</b> spiega posizioni"
+	global state
+	global sensor_choice
+
 	if content_type == 'text':
 		if command == '/start':
 			markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Mappa')], [KeyboardButton(text='S1'), KeyboardButton(text='S2'), KeyboardButton(text='S3'), KeyboardButton(text='S4'), KeyboardButton(text='S5')], [KeyboardButton(text='S6'), KeyboardButton(text='S7'), KeyboardButton(text='S8'), KeyboardButton(text='S9'), KeyboardButton(text='S10')] ])
-			bot.sendMessage(chat_id, "Ciao <b>{0}</b>\nScegli un'opzione:\n<b>S1</b> sta per Sensore 1\n<b>Mappa</b> spiega posizioni".format(msg["from"]["first_name"]), parse_mode = "HTML", reply_markup = markup)		
+			bot.sendMessage(chat_id, "Ciao <b>{0}</b>\n\n".format(msg["from"]["first_name"]) + start_message, parse_mode = "HTML", reply_markup = markup)
 
-		if command == 'S1':
-			global sensor_choice
-			sensor_choice = "Sensore 1"
-			markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Ultimi 15 minuti'), KeyboardButton(text='Ultime 2 ore')]])
+		if command == 'Indietro':
+			markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Mappa')], [KeyboardButton(text='S1'), KeyboardButton(text='S2'), KeyboardButton(text='S3'), KeyboardButton(text='S4'), KeyboardButton(text='S5')], [KeyboardButton(text='S6'), KeyboardButton(text='S7'), KeyboardButton(text='S8'), KeyboardButton(text='S9'), KeyboardButton(text='S10')]])
+			bot.sendMessage(chat_id, start_message, parse_mode = "HTML", reply_markup = markup) 		
+
+		if command == 'S1' or command == 'S2' or command == 'S3' or command == 'S4' or command == 'S5' or command == 'S6' or command == 'S7' or command == 'S8' or command == 'S9' or command == 'S10':
+			markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Ultimi 15 minuti'), KeyboardButton(text='Ultime 2 ore')], [KeyboardButton(text='Ultime 24 ore')], [KeyboardButton(text='Indietro')]])
 			bot.sendMessage(chat_id, "Scegli un'opzione",  reply_markup = markup)
+			if command == 'S1':
+				state = 1
+				sensor_choice = "'Sensore 1'"
+			if command == 'S2':
+				state = 2
+				sensor_choice = "'Sensore 2'"
+			if command == 'S3':
+				state = 3
+				sensor_choice = "'Sensore 3'"
+			if command == 'S4':
+				state = 4
+				sensor_choice = "'Sensore 4'"
+			if command == 'S5': 
+				state = 5
+				sensor_choice = "'Sensore 5'"
+			if command == 'S6':
+				state = 6
+				sensor_choice = "'Sensore 6'"
+			if command == 'S7':
+				state = 7
+				sensor_choice = "'Sensore 7'"
+			if command == 'S8':
+				state = 8
+				sensor_choice = "'Sensore 8'"
+			if command == 'S9':
+				state = 9
+				sensor_choice = "'Sensore 9'"
+			if command == 'S10':
+				state = 10
+				sensor_choice = "'Sensore 10'"
 
 		if command == 'Ultime 2 ore':
 			try:
-				cursor.execute('SELECT * FROM temperaturedata WHERE dateandtime >= DATE_SUB(NOW(),INTERVAL 2 HOUR);')
+				sql_query = "SELECT * FROM temperaturedata WHERE dateandtime >= DATE_SUB(NOW(), INTERVAL 2 HOUR) AND sensor LIKE {0}".format(sensor_choice)
+				cursor.execute(sql_query)
 				results = cursor.fetchall()
 				for row in results:
 					dateandtime = row[0]
 					sensor = row[1]
 					temperature = row[2]
 					humidity = row[3]
-					formatted_string = "Data e ora: \n{0} \nSensore: {1} \nTemperatura: {2} \nUmidita': {3} \n---------------- \n {4}".format(dateandtime, sensor, temperature, humidity, sensor_choice)
+					formatted_string = "Data e ora: \n{0} \nSensore: {1} \nTemperatura: {2} \nUmidita': {3} \n----------------\nState: {4},\nSensor: {5}".format(dateandtime, sensor, temperature, humidity, state, sensor_choice)
 					bot.sendMessage(chat_id, formatted_string)
 			except Exception, e:
 				bot.sendMessage(chat_id, "Errore 101: DB execute error\n\n" + str(e))
 	
 		if command == 'Ultimi 15 minuti':
 			try:
-				cursor.execute('SELECT * FROM temperaturedata WHERE dateandtime >= DATE_SUB(NOW(),INTERVAL 15 MINUTE);')
+				sql_query = "SELECT * FROM temperaturedata WHERE dateandtime >= DATE_SUB(NOW(),INTERVAL 15 MINUTE) AND sensor LIKE {0};".format(sensor_choice)
+				cursor.execute(sql_query)
 				results = cursor.fetchall()
 				for row in results:
 					dateandtime = row[0]
 					sensor = row[1]
 					temperature = row[2]
 					humidity = row[3]
-					formatted_string = "Data e ora: \n{0} \nSensore: {1} \nTemperatura: {2} \nUmidita': {3} \n--------------- \n {4}".format(dateandtime, sensor, temperature, humidity, sensor_choice)
+					formatted_string = "Data e ora: \n{0} \nSensore: {1} \nTemperatura: {2} \nUmidita': {3} \n---------------\nState: {4},\nSensor: {5}".format(dateandtime, sensor, temperature, humidity, state, sensor_choice)
+					bot.sendMessage(chat_id, formatted_string)
+			except Exception, e:
+				bot.sendMessage(chat_id, "Errore 101: DB execute error\n\n" + str(e))
+
+		if command == 'Ultime 24 ore':
+			try:
+				sql_query = "SELECT * FROM temperaturedata WHERE dateandtime >= DATE_SUB(NOW(), INTERVAL 24 HOUR) AND sensor LIKE {0};".format(sensor_choice)
+				cursor.execute(sql_query)
+				results = cursor.fetchall()
+				if len(results) == 0:
+					bot.sendMessage(chat_id, "Errore 10: Empty list\n\nCheck sensors")
+				for row in results:
+					dateandtime = row[0]
+					sensor = row[1]
+					temperature = row[2]
+					humidity = row[3]
+					formatted_string = "Data e ora: \n{0} \nSensore: {1} \nTemperatura: {2} \nUmidita': {3} \n---------------\nState: {4},\nSensor: {5}".format(dateandtime, sensor, temperature, humidity, state, sensor_choice)
 					bot.sendMessage(chat_id, formatted_string)
 			except Exception, e:
 				bot.sendMessage(chat_id, "Errore 101: DB execute error\n\n" + str(e))
 	#Send the answer to the user
 	#bot.sendMessage(chat_id, reply)
+		
 
 
 #Listening
